563f383c3c89f50d2bde7f4f538612e5e006ee9e
wwp <wwp.personal@outlook.com>
2025-07-17 23:00:12 +0800

    添加字体图标

--- src/components/common/Iconfont.vue
+++ src/components/common/Iconfont.vue
@@ -5,9 +5,9 @@
 <script setup lang="ts">
 import { createFromIconfontCN } from '@ant-design/icons-vue'
 
-// const iconUrl = new URL('../../assets/iconfont/iconfont.js', import.meta.url).href
+const iconUrl = new URL('../../assets/iconfont/iconfont.js', import.meta.url).href
 
-const iconUrl = '//at.alicdn.com/t/c/font_4975895_ky5dianhw2s.js'
+// const iconUrl = '//at.alicdn.com/t/c/font_4975895_ky5dianhw2s.js'
 
 const IconFont = createFromIconfontCN({
   scriptUrl: iconUrl,
